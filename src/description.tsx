1f05417c90543c7e59303406b24b2818ba82c6ee
bbbottle <z@zjh.im>
2021-10-09 16:30:44 +0800

    chore: ci test

--- src/description.tsx
+++ src/description.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Description = () => {
   return (
     <cite>
-      "bbki.ng means <b>baby king</b>
+      <b>"bbki.ng</b> means <b>baby king</b>
       just like 'bbbottle' means baby bottle
       <br />
       and has nothing to do with those people who are good at talking [doge]"

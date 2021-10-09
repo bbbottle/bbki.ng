41c11c077decd94aa757bd9c986ff1d341a2298f
bbbottle <z@zjh.im>
2021-10-09 15:22:18 +0800

    chore: add vite and prettier

---
+++ src/description.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Description = () => {
+  return (
+    <cite>
+      "bbki.ng means <b>baby king</b>
+      just like 'bbbottle' means baby bottle
+      <br />
+      and has nothing to do with those people who are good at talking [doge]"
+      <br />
+      <br />- by bbbottle
+    </cite>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Description } from "./description";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Description />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

80842a3a7babc64384d995b0bc81b09efe685401
bbbottle <z@zjh.im>
2021-10-12 22:51:31 +0800

    feat: simple route test

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-import { NavMainLayout } from "./components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Description, NavMainLayout } from "./components";
 import { Cover } from "./components/cover";
 
 export const App = () => {
-  return <NavMainLayout nav={<span>index</span>} main={<Cover />} />;
+  const mainJsx = (
+    <Switch>
+      <Route path="/ext">
+        <Description />
+      </Route>
+      <Route path="/">
+        <Cover />
+      </Route>
+    </Switch>
+  );
+
+  return (
+    <Router>
+      <NavMainLayout
+        nav={
+          <Link to="/">
+            <span>index</span>
+          </Link>
+        }
+        main={mainJsx}
+      />
+    </Router>
+  );
 };
--- src/components/cover/index.tsx
+++ src/components/cover/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ListWithTitle } from "../list";
 
 export const Cover = () => {
   const question = "已满十八岁？";
   const answers = [
-    <span className="underline text-red-500">YES</span>,
+    <Link to="/ext">
+      <span className="underline text-red-500">YES</span>
+    </Link>,
     <span className="text-gray-200">NO</span>,
   ];
 

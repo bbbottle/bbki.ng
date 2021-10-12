4a5410304c1bf464342b1709bb3cfa6a971c1aa9
bbbottle <z@zjh.im>
2021-10-12 20:44:24 +0800

    chore(tailwind.config.js): customize font family config of tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        "pt-sans",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
   },
   variants: {
     extend: {},

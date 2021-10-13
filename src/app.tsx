65f3ee4c859fea09d6f93058925ec46bb8cecdd8
bbbottle <z@zjh.im>
2021-10-13 21:52:25 +0800

    fix: got 404 page after refresh under bbki.ng/ext #1

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Footer, NavMainLayout, Nav, Cover } from "./components";
 import { Extensions } from "./pages/extensions";
 

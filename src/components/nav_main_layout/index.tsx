81890f9bc97747804100429f192d4ebe0d1898cb
bbbottle <z@zjh.im>
2021-10-13 20:00:44 +0800

    feat(component/layout): support optional footer

--- src/components/nav_main_layout/index.tsx
+++ src/components/nav_main_layout/index.tsx
@@ -3,18 +3,24 @@ import React, { ReactElement } from "react";
 type NavMainLayoutProps = {
   nav?: ReactElement;
   main: ReactElement;
+  footer?: ReactElement;
 };
 
 export const NavMainLayout = (props: NavMainLayoutProps) => {
-  const { nav, main } = props;
+  const { nav, main, footer } = props;
   return (
     <main className="flex flex-col h-full">
-      <nav className="flex-grow-0 flex-shrink-0 border-1 border-black p-10 flex items-center">
+      <nav className="flex-grow-0 flex-shrink-0 p-10 flex items-center">
         {nav}
       </nav>
-      <section className="flex-grow flex-shrink-0 border-1 border-gray items-center justify-center flex p-10">
+      <section className="flex-grow flex-shrink-0 items-center justify-center flex p-10">
         {main}
       </section>
+      {footer && (
+        <footer className="flex-grow-0 flex-shrink-0 flex items-center justify-center p-6">
+          {footer}
+        </footer>
+      )}
     </main>
   );
 };

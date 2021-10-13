52cfdeb8300b7d6b5b19119f2b53c3eb02830cd8
bbbottle <z@zjh.im>
2021-10-13 20:01:17 +0800

    feat(component/link): support external link and new color

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -3,11 +3,28 @@ import { Link as BaseLink, LinkProps as BaseLinkProps } from "react-router-dom";
 import React from "react";
 
 interface LinkProps extends BaseLinkProps {
-  color?: "blue" | "red";
+  color?: "blue" | "red" | "gray";
+  external?: boolean;
+  to: string;
 }
 
+const COLOR_CLS_MAP = {
+  blue: "text-blue-600",
+  red: "text-red-500",
+  gray: "text-gray-400",
+};
+
 export const Link = (props: LinkProps) => {
-  const { color = "blue" } = props;
-  const colorCls = color === "blue" ? "text-blue-600" : "text-red-500";
-  return <BaseLink {...props} className={cls(colorCls, "underline")} />;
+  const { color = "blue", external } = props;
+  const colorCls = COLOR_CLS_MAP[color];
+  const linkCls = cls(colorCls, "underline");
+  if (external) {
+    return (
+      <a href={props.to} className={linkCls} target="_blank">
+        {props.children}
+      </a>
+    );
+  }
+
+  return <BaseLink {...props} className={linkCls} />;
 };

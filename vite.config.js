7353a0b25ea4108f537f283765e4b9fc8f32459e
bbbottle <z@zjh.im>
2021-10-15 09:26:27 +0800

    chore(mdx): support mdx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import mdx from "vite-plugin-mdx";
 import react from "@vitejs/plugin-react";
+
+const options = {
+  // See https://mdxjs.com/advanced/plugins
+  remarkPlugins: [
+    // E.g. `remark-frontmatter`
+  ],
+  rehypePlugins: [],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdx(options)],
 });

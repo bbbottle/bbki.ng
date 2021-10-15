2ff5dd9ae94fd2f54f55dc37694c8a7bf5393081
bbbottle <z@zjh.im>
2021-10-15 09:27:14 +0800

    refactor(ext/ext): mdx test

--- src/pages/extensions/txt/index.tsx
+++ src/pages/extensions/txt/index.tsx
@@ -1,19 +1,11 @@
 import React from "react";
+import Poem from "./poem.mdx";
 import { AlmostCenterLayout } from "../../../components";
 
 export const Txt = () => {
   return (
     <AlmostCenterLayout>
-      <blockquote>
-        河流用一生梳理 <br />
-        地球的表情 <br />
-        沿岸收集人類的喜怒哀樂 <br />
-        檔案堆滿了海底的家 <br />
-        <br />
-        — 《河流紀事》鄧獻誌
-        <br />
-        <br />
-      </blockquote>
+      <Poem />
     </AlmostCenterLayout>
   );
 };

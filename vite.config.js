c01de5fb47ea3409ef768267c332c863ade0bac8
bbbottle <z@zjh.im>
2021-10-15 11:27:37 +0800

    chore: add path alias

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
   plugins: [react()],
 });

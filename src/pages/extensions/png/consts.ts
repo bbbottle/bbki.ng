1144efafb42864178a822309fe209b5a0ad5f3c4
bbbottle <z@zjh.im>
2021-10-17 21:14:45 +0800

    feat(png): new photos

--- src/pages/extensions/png/consts.ts
+++ src/pages/extensions/png/consts.ts
@@ -1,12 +1,12 @@
 export const JPGProjectList = [
   {
-    path: "/ext/png/光前村系列",
-    name: "光前村系列",
+    path: "/ext/png/敬县城",
+    name: "敬县城",
     imgList: [
-      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/photo_2021-07-04_17-03-43.jpg",
-      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1668.JPG",
-      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/LFKW1496.JPG",
-      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1677.JPG",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/3922C86B-79D5-455F-9FA8-7EB405CE95A6-38852-000007863DB50E30.jpg",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/B3AACBCA-807B-44C0-9CA2-E1F7E0C00C93_black.jpg",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/E2FDA467-AD6D-4410-9B50-66E1624C13D6-38852-0000078630EDA7C4.jpg",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/IMG_3869.JPG",
     ],
   },
   {
@@ -16,6 +16,17 @@ export const JPGProjectList = [
       "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2693870407.webp",
       "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2693886443.webp",
       "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2693870393.webp",
+    ],
+  },
+  {
+    path: "/ext/png/光前村系列",
+    name: "光前村系列",
+    imgList: [
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_1844.jpg",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/photo_2021-07-04_17-03-43.jpg",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1668.JPG",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/LFKW1496.JPG",
+      "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1677.JPG",
     ],
   },
 ];

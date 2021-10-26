bd59e3961653ad4564b621cf14a11b31b6c49b07
bbbottle <z@zjh.im>
2021-10-26 19:53:43 +0800

    refactor(ext/consts): seprate into config and code

--- src/pages/extensions/avi/consts.ts
+++ src/pages/extensions/avi/consts.ts
@@ -1,29 +1,10 @@
 import { VideoPlayer, withVideoWrapper } from "@/components";
+import { ROUTES, VIDEO_LOGS } from "@/constants";
 
 const component = withVideoWrapper(VideoPlayer);
-export const Logs = [
-  {
-    path: "/ext/avi/2021-09-10",
-    name: "2021-09-10",
-    component,
-    componentProps: {
-      src: "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/video/2021-09-10",
-    },
-  },
-  {
-    path: "/ext/avi/2021-09-18",
-    name: "2021-09-18",
-    component,
-    componentProps: {
-      src: "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/video/2021-09-18.mov",
-    },
-  },
-  {
-    path: "/ext/avi/2021-09-28",
-    name: "2021-09-28",
-    component,
-    componentProps: {
-      src: "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/video/2021-09-28-4k30H.mov",
-    },
-  },
-];
+export const Logs = VIDEO_LOGS.map(({ name, src }) => ({
+  path: `${ROUTES.AVI}/${name}`,
+  name,
+  component,
+  componentProps: { src },
+}));
--- src/pages/extensions/png/consts.ts
+++ src/pages/extensions/png/consts.ts
@@ -1,50 +1,12 @@
 import { ImgList } from "@/components";
-import { addOssWebpProcessSuffix } from "@/utils";
+import { PHOTO_PROJECTS, ROUTES } from "@/constants";
 
-export const ProjectList = [
-  {
-    path: "/ext/png/敬县城",
-    name: "敬县城",
-    component: ImgList,
-    componentProps: {
-      className: "mt-40",
-      srcList: [
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/4688FF3F-8EF5-4F63-B726-9AC275C1A97E-50268-000009856F7D5885.jpg",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/3922C86B-79D5-455F-9FA8-7EB405CE95A6-38852-000007863DB50E30.jpg",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/B3AACBCA-807B-44C0-9CA2-E1F7E0C00C93_black.jpg",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/E2FDA467-AD6D-4410-9B50-66E1624C13D6-38852-0000078630EDA7C4.jpg",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/county-town/IMG_3869.JPG",
-      ],
-    },
+export const ProjectList = PHOTO_PROJECTS.map(({ name, images: srcList }) => ({
+  path: `${ROUTES.PNG}/${name}`,
+  name,
+  component: ImgList,
+  componentProps: {
+    className: "mt-40",
+    srcList,
   },
-  {
-    path: "/ext/png/河东河西",
-    name: "河东河西",
-    component: ImgList,
-    componentProps: {
-      className: "mt-40",
-      srcList: [
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2705047873.webp",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2704851973.webp",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/p2705529760.webp",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20211025194518.jpg",
-        addOssWebpProcessSuffix(
-          "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/xiang-jiang-river/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20211025202204.jpg"
-        ),
-      ],
-    },
-  },
-  {
-    path: "/ext/png/光前村",
-    name: "光前村",
-    component: ImgList,
-    componentProps: {
-      className: "mt-40",
-      srcList: [
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_1844.jpg",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1668.JPG",
-        "https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/before-the-light/IMG_E1677.JPG",
-      ],
-    },
-  },
-];
+}));

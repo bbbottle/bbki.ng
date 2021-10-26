4cdf78195724e986640962932688ba212f1f08b5
bbbottle <z@zjh.im>
2021-10-26 19:07:58 +0800

    chore(app): change to browser router

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Footer, NavMainLayout, Nav, HotKeyNav } from "./components";
 import { Extensions, Cover } from "./pages";
 

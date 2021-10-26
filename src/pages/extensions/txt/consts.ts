5b2f70c90fe914827ca8013148a6a2f12494dd82
bbbottle <z@zjh.im>
2021-10-26 14:06:33 +0800

    refactor(articles): import / export and const file

--- src/pages/extensions/txt/consts.ts
+++ src/pages/extensions/txt/consts.ts
@@ -1,53 +1,12 @@
 import { compPathObj } from "@/types/path";
 import { withArticleWrapper } from "@/components";
-import River from "@/articles/river.mdx";
-import Letter from "@/articles/letter.mdx";
-import Shortcuts from "@/articles/shortcuts.mdx";
-import Travel from "@/articles/travel.mdx";
-import CoolDown from "@/articles/cooldown.mdx";
-import Move from "@/articles/move.mdx";
-import LogoGuidelines from "@/articles/logo-guidelines.mdx";
-import Dreams from "@/articles/dreams.mdx";
+import { ROUTES } from "@/constants";
+import * as articles from "@/articles";
 
-export const ArticleList: compPathObj[] = [
-  {
-    path: "/ext/txt/快捷键",
-    name: "快捷键",
-    component: withArticleWrapper(Shortcuts),
-  },
-  {
-    path: "/ext/txt/河流纪事",
-    name: "河流纪事",
-    component: withArticleWrapper(River),
-  },
-  {
-    path: "/ext/txt/信",
-    name: "信",
-    component: withArticleWrapper(Letter),
-  },
-  {
-    path: "/ext/txt/离开",
-    name: "离开",
-    component: withArticleWrapper(Travel),
-  },
-  {
-    path: "/ext/txt/降温",
-    name: "降温",
-    component: withArticleWrapper(CoolDown),
-  },
-  {
-    path: "/ext/txt/古老的小区",
-    name: "古老的小区",
-    component: withArticleWrapper(Move),
-  },
-  {
-    path: "/ext/txt/网站标识使用规范",
-    name: "网站标识使用规范",
-    component: withArticleWrapper(LogoGuidelines),
-  },
-  {
-    path: "/ext/txt/梦里的事",
-    name: "梦里的事",
-    component: withArticleWrapper(Dreams),
-  },
-];
+export const ArticleList: compPathObj[] = Object.entries(articles).map(
+  ([name, component]) => ({
+    path: `${ROUTES.TXT}/${name}`,
+    name,
+    component: withArticleWrapper(component),
+  })
+);

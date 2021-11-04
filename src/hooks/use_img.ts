9ab40b1ef8bdde03374c027d2dc5e751cff95bbb
bbbottle <z@zjh.im>
2021-11-04 09:13:01 +0800

    enhance(useImage): return img element instead of loading flag only.

--- src/hooks/use_img.ts
+++ src/hooks/use_img.ts
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 
-export const useImgLoading = (src: string) => {
+export const useImg = (src: string) => {
   const [loading, setLoading] = useState(true);
+  const [img, setImg] = useState<HTMLImageElement | null>(null);
   useEffect(() => {
     let img = new Image();
+    img.crossOrigin = "Anonymous";
     img.src = src;
     img.onload = () => {
       setLoading(false);
+      setImg(img);
     };
     return () => {
       img.src = "";
     };
   }, []);
-  return loading;
+
+  return { img, loading };
 };

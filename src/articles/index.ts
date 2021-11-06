3bb5f5ca6a31dc24b0d9d75aba4d6dc705302f5f
bbbottle <z@zjh.im>
2021-11-06 22:20:01 +0800

    refactor(articles): update txt entries

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,8 +1,4 @@
-export { default as 网站快捷键 } from "./shortcuts.mdx";
-export { default as 河流纪事 } from "./river.mdx";
-export { default as 信 } from "./letter.mdx";
+export { default as 网站说明书 } from "./bbking-manual.mdx";
 export { default as 离开 } from "./travel.mdx";
 export { default as 降温 } from "./cooldown.mdx";
-export { default as 古老的小区 } from "./move.mdx";
-export { default as 网站标识使用指南 } from "./logo-guidelines.mdx";
-export { default as 梦里的事 } from "./dreams.mdx";
+export { default as 与或非禁区 } from "./anti-logic.mdx";

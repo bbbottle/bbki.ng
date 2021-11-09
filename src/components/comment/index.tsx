a8db55c9b79216a79d30462d0c53c8d8ae4db391
bbbottle <z@zjh.im>
2021-11-09 08:59:23 +0800

    refactor(cusdis): update attrs

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import classnames from "classnames";
-import { useCusdisAttr } from "@/hooks/use_cusdis_attr";
 import { ReactCusdis } from "react-cusdis";
 import { BgColors } from "@/types/color";
 import { useIframeReady } from "@/hooks/use_iframe_ready";
+import { CUSDIS_ATTRS } from "@/constants/cusdis";
 
 type CommentProps = {
   title: string;
 };
 
 export const Comment = (props: CommentProps) => {
-  const cusdisAttrs = useCusdisAttr(props.title);
+  const path = location.href;
+  const cusdisAttrs = Object.assign({}, CUSDIS_ATTRS, {
+    pageTitle: props.title,
+    pageUrl: path,
+    pageId: path,
+  });
+
   const ready = useIframeReady(3000);
+
   return (
     <div
       className={classnames(

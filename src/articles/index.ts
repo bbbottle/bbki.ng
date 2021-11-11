cc49af93b04ee3110f97a4598b73f356089ced52
bbbottle <z@zjh.im>
2021-11-11 19:28:59 +0800

    feat(article): add new article

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -2,5 +2,12 @@ import * as 网站说明书 from "./bbking-manual.mdx";
 import * as 离开 from "./travel.mdx";
 import * as 降温 from "./cooldown.mdx";
 import * as 与或非禁区 from "./anti-logic.mdx";
+import * as 没有女人的男人们 from "./men-without-women.mdx";
 
-export const MdxArticleList = [网站说明书, 与或非禁区, 离开, 降温];
+export const MdxArticleList = [
+  网站说明书,
+  与或非禁区,
+  离开,
+  降温,
+  没有女人的男人们,
+];

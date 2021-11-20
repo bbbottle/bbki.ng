b170007f9a8eec2026859a94889ca5cbe882a8c3
bbbottle <z@zjh.im>
2021-11-20 10:47:38 +0800

    feat(articles): add new article

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -4,9 +4,11 @@ import * as 降温 from "./cooldown.mdx";
 import * as 与或非禁区 from "./anti-logic.mdx";
 import * as 没有女人的男人们 from "./men-without-women.mdx";
 import * as 一天计划 from "./movie-day.mdx";
+import * as 网站图片展示 from "./img.mdx";
 
 export const MdxArticleList = [
   网站说明书,
+  网站图片展示,
   与或非禁区,
   离开,
   降温,

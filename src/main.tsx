878ff58026709b0e79a45402a1062756ffdd002a
bbbottle <z@zjh.im>
2021-12-06 13:54:52 +0800

    chore(sentry): add version

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { getEnv } from "@/utils";
 Sentry.init({
   dsn: "https://6c48bed663f24c78ad9ccb5754854b85@o1084530.ingest.sentry.io/6094373",
   integrations: [new Integrations.BrowserTracing()],
-
+  release: "0.0.1",
   environment: getEnv(),
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.

b1b2e31e2eef4ff450a6f9b381a82c3cb7b12e68
bbbottle <z@zjh.im>
2021-12-06 10:45:21 +0800

    chore(sentry): add sentry monitor

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { ReloadPrompt } from "@/components";
 import { App } from "./app";
 import "./main.css";
+
+Sentry.init({
+  dsn: "https://6c48bed663f24c78ad9ccb5754854b85@o1084530.ingest.sentry.io/6094373",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>

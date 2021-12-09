b2ec73954aca8ceb6ce915fd965aae5ce844c997
bbbottle <z@zjh.im>
2021-12-09 10:01:09 +0800

    chore(sentry): add routing instrumentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import { RouterHistory } from "@sentry/react/dist/reactrouter";
+import { createBrowserHistory } from "history";
 import { ReloadPrompt } from "@/components";
 import App from "./app";
 import "./main.css";
 import { getEnv } from "@/utils";
 
+const history: unknown = createBrowserHistory();
+
 Sentry.init({
   dsn: "https://6c48bed663f24c78ad9ccb5754854b85@o1084530.ingest.sentry.io/6094373",
-  integrations: [new Integrations.BrowserTracing()],
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV5Instrumentation(
+        history as RouterHistory
+      ),
+    }),
+  ],
   release: "0.0.1",
   environment: getEnv(),
   // Set tracesSampleRate to 1.0 to capture 100%

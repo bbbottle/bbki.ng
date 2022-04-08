a0456dbf316ca98df881ef40b0d24f679f294212
bbbottle <z@zjh.im>
2022-04-08 21:42:44 +0800

    feat: add new sticker

--- src/components/stickers/index.tsx
+++ src/components/stickers/index.tsx
@@ -3,7 +3,7 @@ import { Img } from "@/components";
 
 export const Stickers = () => {
   return (
-    <div className="fixed bottom-256 right-128">
+    <div className="fixed bottom-256 right-64">
       <Img
         size="large"
         src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/sticker-water-delivery.jpg"
@@ -19,6 +19,14 @@ export const Stickers = () => {
         width={93}
         height={50}
         renderedWidth={93}
+      />
+      <Img
+        size="large"
+        src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/bar-code.jpg"
+        className="-rotate-[13.37deg] relative top-128 right-256"
+        width={176}
+        height={60}
+        renderedWidth={176}
       />
     </div>
   );

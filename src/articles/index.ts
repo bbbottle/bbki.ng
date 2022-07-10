fe2e38751d825423a0eb2bf71c5398d2babb09a1
bbbottle <z@zjh.im>
2022-07-10 23:50:40 +0800

    feat: new article

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -12,6 +12,7 @@ import * as 大寒 from "./major-cold.mdx";
 import * as 春雨 from "./spring-rain.mdx";
 import * as 春寒 from "./spring-cooldown.mdx";
 import * as 入夏 from "./web-burnning.mdx";
+import * as 六月 from "./black-river.mdx";
 
 export const MdxArticleList = [
   说明书,
@@ -28,4 +29,5 @@ export const MdxArticleList = [
   春雨,
   春寒,
   入夏,
+  六月,
 ];

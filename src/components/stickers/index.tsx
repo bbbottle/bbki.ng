ffd4e64d5f3350fc1a90e8fce03553c99e57a355
bbbottle <z@zjh.im>
2022-07-15 20:51:34 +0800

    fix: update cover

--- src/components/stickers/index.tsx
+++ src/components/stickers/index.tsx
@@ -1,31 +1,41 @@
 import React from "react";
 import { Img } from "@bbki.ng/components";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export const Stickers = () => {
+  const { pathname } = useLocation();
+
+  if (pathname !== "/") {
+    return null;
+  }
+
   return (
-    <div className="fixed bottom-256 right-64 invisible xl:visible z-[1000]">
-      <Img
-        size="large"
-        src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/sticker-water-delivery.jpg"
-        className="-rotate-[32.95deg] relative -top-64"
-        width={126}
-        height={59}
-        renderedWidth={126}
-      />
-      <Img
-        size="large"
-        src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/sticker-lock.jpg"
-        className="rotate-[10.77deg]"
-        width={93}
-        height={50}
-        renderedWidth={93}
-      />
+    <div className="fixed bottom-128 md:bottom-256 right-16 z-10 md:right-64">
+      <NavLink to="projects">
+        <Img
+          size="large"
+          src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/sticker-water-delivery.jpg"
+          className="-rotate-[32.95deg] relative top-0 md:-top-64 left-64 md:left-[unset]"
+          width={126}
+          height={59}
+          renderedWidth={126}
+        />
+      </NavLink>
+      <NavLink to="404">
+        <Img
+          size="large"
+          src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/sticker-lock.jpg"
+          className="rotate-[10.77deg] relative left-64 md:left-[unset]"
+          width={93}
+          height={50}
+          renderedWidth={93}
+        />
+      </NavLink>
       <NavLink to="blog">
         <Img
           size="large"
           src="https://zjh-im-res.oss-cn-shenzhen.aliyuncs.com/image/stickers/bar-code.jpg"
-          className="-rotate-[13.37deg] relative top-128 right-256"
+          className="-rotate-[13.37deg] relative top-64 left-64 md:left-[unset] md:right-256 md:top-128"
           width={176}
           height={60}
           renderedWidth={176}
--- src/pages/cover/index.tsx
+++ src/pages/cover/index.tsx
@@ -1,38 +1,10 @@
 import React from "react";
-import { DisabledText } from "@/components";
-import { Link, LinkColor, Article } from "@bbki.ng/components";
+import { Article } from "@bbki.ng/components";
 
 export const Cover = (props: { className: string }) => {
   return (
-    <Article title="十八歳未満？" className={props.className}>
-      <DisabledText>
-        <p>
-          请注意，您尝试访问的网站包含儿童内容。 任何年满 18
-          岁的人士均不得访问本网站，并应立即离开。本网站的内容是根据 BBKi.ng
-          的法律创建和管理的。根据国家或地区的不同，即使您未满 18
-          岁，访问该网站也可能是非法的。访问本网站应由您自行判断并承担风险，对于您访问本网站的违法行为，BBKi.ng
-          不承担任何责任或义务。您想访问该网站吗？
-          <Link to="/projects" color={LinkColor.RED} style={{ padding: 4 }}>
-            是
-          </Link>
-        </p>
-        <br />
-        <p>
-          Please be advised that the website you are attempting to visit
-          contains child content. Anybody above the age of 18 is prohibited from
-          accessing the website, and should immediately leave from this website.
-          The content of the website is created and managed under the BBKi.ng's
-          laws. Depending on the countries or areas, accessing the website may
-          be illegal, even in the event that you are at the age of 18 or
-          younger. Accessing the website shall be done at your own judgement and
-          risk, and BBKi.ng disclaims responsibility or liability in the event
-          of your violation of laws by accessing the website. Do you wish to
-          access the website?&nbsp;
-          <Link to="/projects" color={LinkColor.RED} style={{ padding: 4 }}>
-            Yes
-          </Link>
-        </p>
-      </DisabledText>
+    <Article title="">
+      <div className="bg-white">This area intentionally left blank</div>
     </Article>
   );
 };

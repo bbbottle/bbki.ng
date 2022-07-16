17c005e21b5413f27b511d9ab31388a4d3c230a4
bbbottle <z@zjh.im>
2022-07-17 00:11:30 +0800

    fix: cover text

--- src/pages/cover/index.tsx
+++ src/pages/cover/index.tsx
@@ -4,7 +4,7 @@ import { Article } from "@bbki.ng/components";
 export const Cover = (props: { className: string }) => {
   return (
     <Article title="">
-      <div className="bg-white">This area intentionally left blank</div>
+      <div className="bg-white">This page intentionally left blank</div>
     </Article>
   );
 };
